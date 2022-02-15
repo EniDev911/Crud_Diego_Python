c304b19b2b6b3dc9cc94abf6a7e788aa4c7b938c
enidev911 <enidev911@gmail.com>
2022-02-15 19:10:14 +0100

    test

--- test.py
+++ test.py
@@ -1,20 +1,22 @@
 import mysql.connector as mysql 
+from mysql.connector import errorcode
+
+config = {
+	'user': 'ul35rprgfokcahwh',
+	'password': 'KIhphdZ54bTcAVYmtqOk',
+	'host': 'bfgvnwc7zu9xkf2qtqpx-mysql.services.clever-cloud.com'
+}
 
 try:
-	con = mysql.connect(
-			user = 'root',
-			passwd = 'enidev911',
-			port= 3306,
-			db = 'inventario_python')
-
-	cursor = con.cursor()
-	sql = 'DESCRIBE curso;'
-	result = cursor.execute(sql)
-	print(result)
-	print(con.is_connected())
+	cnx = mysql.connect(**config)
+	print("Se establecio la conexión")
 
 except mysql.Error as err:
-	print(err)
-
-finally:
-	con.close()
+	if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+		print("Problem in your connection")
+	elif err.errno == errorcode.ER_BAD_DB_ERROR:
+		print("Database not exists!")
+	else: 
+		print(err)
+else:
+	cnx.close()
